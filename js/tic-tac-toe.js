7d99423f151026439333a70c6f46df7b65f42fa2
MariusTBjercke <marius@bjerckemedia.no>
2021-08-22 20:51:35 +0200

    Added some Norwegian comments to the tic tac toe game

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,11 +1,15 @@
 $(function() {
     
+    // Finn elementet der spillets status skal stå
     var statusDisplay = document.getElementById("game-status");
 
+    // Sjekk om spillet er aktivt for funksjoner nedenfor
     var gameActive = true;
 
+    // Start med spiller "X", viktig at spilleren også blir definert her
     var currentPlayer = "X";
 
+    // Start spillet med tomme felt
     var gameState = [
         "",
         "",
@@ -18,12 +22,15 @@ $(function() {
         ""
     ];
 
+    // Variabler for beskjeder som dukker opp underveis mens man spiller
     var winningMessage = () => "Spiller " + currentPlayer + " har vunnet!";
     var drawMessage = () => "Spillet endte likt!";
     var currentPlayerTurn = () => "Det er " + currentPlayer + " sin tur.";
 
+    // Sett status til hvilken spillers tur det er
     statusDisplay.innerHTML = currentPlayerTurn();
 
+    // Funksjon for celleklikk
     function handleCellPlayed(clickedCell, clickedCellIndex) {
 
         gameState[clickedCellIndex] = currentPlayer;
@@ -31,6 +38,7 @@ $(function() {
 
     }
 
+    // Funksjon for å bytte spiller etter hver runde
     function handlePlayerChange() {
 
         currentPlayer = currentPlayer === "X" ? "O" : "X";
@@ -38,6 +46,7 @@ $(function() {
 
     }
 
+    // Her er det definert hvilke rad-kombinasjoner man kan ha for å vinne
     var winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -83,10 +92,12 @@ $(function() {
 
     }
 
+    // Funksjon for hva som skjer når man trykker på de forskjellige cellene
     function handleCellClick(clickedCellEvent) {
 
         var clickedCell = clickedCellEvent.target;
 
+        // Her henter vi informasjon fra f.eks <div data-cell-index="0"></div> og setter det som en variabel
         var clickedCellIndex = parseInt(clickedCell.getAttribute("data-cell-index"));
 
         if (gameState[clickedCellIndex] !== "" || !gameActive) {
@@ -98,10 +109,16 @@ $(function() {
 
     }
 
+    // Funksjon som gjør at man kan restarte spillet ved å trykke på restart-knappen
     function handleRestartGame() {
 
+        // Sett spillets status tilbake til aktiv
         gameActive = true;
+
+        // Sett spiller til X ved restart
         currentPlayer = "X";
+
+        // Her resetter vi alle boksene
         gameState = [
             "",
             "",
@@ -113,12 +130,16 @@ $(function() {
             "",
             ""
         ];
+
         statusDisplay.innerHTML = currentPlayerTurn();
         document.querySelectorAll(".flexbox-container div").forEach(cell => cell.innerHTML = "");
 
     }
 
+    // Loop gjennom alle celler og legg til et klikk-event slik at vi kan hente informasjon fra cellen.
     document.querySelectorAll(".flexbox-container div").forEach(cell => cell.addEventListener("click", handleCellClick));
+ 
+    // Klikk-event (type onclick) for å restarte spillet. Kjør funksjonen handleRestartGame når man trykker på restart-knappen.
     document.querySelector(".game-restart").addEventListener("click", handleRestartGame);
 
 });
