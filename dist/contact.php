31f05ce0d23dd6a7308679ec7bc10f492ece7ba4
MariusTBjercke <marius@bjerckemedia.no>
2022-02-07 19:12:01 +0100

    Added contact-page

--- dist/contact.php
+++ dist/contact.php
@@ -5,8 +5,21 @@ include 'header.php';
 
 <main>
 
-    <article>
-
+    <article class="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col about-text">
+                    <h2>Kontakt meg</h2>
+                    <h6>Ikke nøl med å ta kontakt!<br>Svarer som regel innen 24 timer.</h6>
+                    <h5>E-post: <a href="mailto:marius@bjerckemedia.no">marius@bjerckemedia.no</a></h5>
+                    <div class="contact-btns">
+                        <a href="https://github.com/MariusTBjercke" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+                        <a href="https://www.linkedin.com/in/marius-toresen-bjercke-49688a62/" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a>
+                        <a href="https://www.facebook.com/marius.t.bjercke/" target="_blank"><i class="fab fa-facebook-square"></i> Facebook</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </article>
 
 </main>
